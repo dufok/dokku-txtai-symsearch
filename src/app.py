022669d125067854d539b45ce54c98720b30ba2d
Stepan Vladovskiy <stepan.vladovskiy@gmail.com>
2025-03-24 20:59:38 -0300

    debug(app.py): trying to find operator that dont exist

--- src/app.py
+++ src/app.py
@@ -355,9 +355,9 @@ def search(req: SearchRequest):
             ),
             semantic AS (
                 SELECT id,
-                       (1 - (embedding <#> :query_embedding)) AS sem_score
+                       (1 - (embedding <#> :query_embedding::vector(384))) AS sem_score
                 FROM documents
-                ORDER BY embedding <#> :query_embedding ASC
+                ORDER BY embedding <#> :query_embedding::vector(384) ASC
                 LIMIT 50
             )
         SELECT d.id, d.content,
